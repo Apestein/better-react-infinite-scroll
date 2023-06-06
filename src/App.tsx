839fd747ff979fe4d0eb2e9b688b2c56cfef8b66
Apestein <ltn2057@protonmail.com>
2023-06-06 01:06:10 -0400

    save point

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import InfiniteScroller from "../package/index"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const elements = [1, 2, 3, 4, 5]
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>test</h1>
+        <InfiniteScroller
+          fetchNextPage={() => {
+            console.log("fetched next page")
+          }}
+          hasNextPage={true}
+          loadingMessage={<p>Loading...</p>}
+          endingMessage={<p>The beginning of time...</p>}
+        >
+          {elements.map((el) => (
+            <li>{el}</li>
+          ))}
+        </InfiniteScroller>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
